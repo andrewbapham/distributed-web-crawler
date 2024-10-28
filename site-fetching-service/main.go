a4d1e97a687efd911d58c89c419fcd9194c0c919
Andrew Pham <andrewbapham@outlook.com>
2024-10-28 19:13:24 -0400

    Add create/update to mongo on site fetch

--- site-fetching-service/main.go
+++ site-fetching-service/main.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
-	"strings"
-	"sync"
+	"net/url"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/twmb/franz-go/pkg/kgo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type MongoCollection struct {
+	collection *mongo.Collection
+}
+
+type KafkaClient struct {
+	client *kgo.Client
+}
+
+type SiteRecord struct {
+	URL           string `bson:"url"`
+	Hash          string `bson:"hash"`
+	LastFetched   int64  `bson:"last_fetched"`
+	LastUpdated   int64  `bson:"last_updated"`
+	BackLinkCount int    `bson:"back_link_count"`
+}
+
+// mocking kafka stream of websites to visit
 var sites = []string{
 	"http://thophuongha.com/MuiSuaMe.html",
 	"http://thophuongha.com/AnhSangLaDay.html",
@@ -22,19 +48,74 @@ func getWebsiteData(link string) string {
 	// fetch website data
 	res, err := http.Get(link)
 	if err != nil {
-		fmt.Printf("failed to fetch website data: %v", err)
+		log.Fatal("failed to fetch website data: %v", err)
 	}
 	content, err := io.ReadAll(res.Body)
 	res.Body.Close()
 	if err != nil {
-		fmt.Printf("failed to read website data: %v", err)
+		log.Fatal("failed to read website data: %v", err)
 	}
 	return string(content)
 }
-func main() {
-	seeds := []string{"localhost:9092"}
 
-	client, err := kgo.NewClient(
+func handleSiteFromQueue(link string, mongoClient *MongoCollection, kafkaClient *kgo.Client) {
+	// fetch website data from kafka queue
+	html := getWebsiteData(link)
+
+	h := sha256.New()
+	h.Write([]byte(html))
+	htmlHash := base64.URLEncoding.EncodeToString(h.Sum(nil))
+
+	// check if URL and hash already exist in database, and if not, insert and add S3 object
+	// if hash is different, update the hash and S3 object
+	fmt.Println("hash: ", htmlHash)
+
+	newRecord := SiteRecord{
+		URL:         link,
+		Hash:        htmlHash,
+		LastFetched: time.Now().Unix(),
+	}
+
+	var existingRecord SiteRecord
+	res := mongoClient.collection.FindOne(context.Background(), bson.D{{"url", link}})
+	if res.Err() != nil && errors.Is(res.Err(), mongo.ErrNoDocuments) {
+		fmt.Println("record not found")
+		result, err := mongoClient.collection.InsertOne(context.Background(), newRecord)
+		if err != nil {
+			log.Fatal("failed to insert record: %v", err)
+		}
+		fmt.Println("record inserted: ", result.InsertedID)
+	} else if res.Err() != nil {
+		log.Fatal("record fetch failed: %v", res.Err())
+	} else {
+		// record found
+		res.Decode(&existingRecord)
+
+		if existingRecord.Hash != htmlHash {
+			fmt.Println("Hashes don't match, updating...")
+			// update hash
+			valuesToUpdate := bson.D{{Key: "hash", Value: htmlHash}, {Key: "last_fetched", Value: time.Now().Unix()}}
+			_, err := mongoClient.collection.UpdateOne(context.Background(), bson.D{{Key: "url", Value: link}}, bson.D{{Key: "$set", Value: valuesToUpdate}})
+
+			if err != nil {
+				log.Fatal("failed to update record: %v", err)
+			}
+		}
+	}
+	//res, _ := bson.MarshalExtJSON(result, false, false)
+	//fmt.Println(string(res))
+
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	seeds := []string{os.Getenv("KAFKA_BROKER")}
+
+	kafkaClient, err := kgo.NewClient(
 		kgo.SeedBrokers(seeds...),
 		kgo.ConsumerGroup("site-fetching-service"),
 		kgo.ConsumeTopics("websites"),
@@ -42,34 +123,40 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer client.Close()
+	defer kafkaClient.Close()
+
+	fmt.Println("Kafka client connected")
 
 	ctx := context.Background()
 
-	var wg sync.WaitGroup
-	// record := &kgo.Record{Topic: "kafka-test", Value: []byte("hello world")}
-	// client.Produce(ctx, record, func(r *kgo.Record, err error) {
-	// 	defer wg.Done()
-	// 	if err != nil {
-	// 		fmt.Printf("record produce failed: %v\n", err)
-	// 	}
-	// })
-
-	for _, site := range sites {
-		websiteData := getWebsiteData(site)
-		// get first line of websiteData
-		firstLine := strings.Join(strings.Split(websiteData, "\n")[0:6], "\n")
-
-		record := &kgo.Record{Topic: "kafka-test", Value: []byte("site: " + site + " - " + firstLine)}
-		wg.Add(1)
-		client.Produce(ctx, record, func(r *kgo.Record, err error) {
-			defer wg.Done()
-			if err != nil {
-				fmt.Printf("record produce failed: %v\n", err)
-			}
-		})
+	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(os.Getenv("MONGO_URL")))
+	if err != nil {
+		log.Fatal("failed to connect to mongo: %v\n", err)
 	}
+	defer mongoClient.Disconnect(ctx)
 
-	wg.Wait()
-	fmt.Println("done parsing all sites")
+	fmt.Println("Mongo client connected")
+
+	sitesCollection := MongoCollection{collection: mongoClient.Database(os.Getenv("MONGO_DB_NAME")).Collection(os.Getenv("MONGO_COLLECTION_NAME"))}
+
+	// Listen to incoming site queue and fetch data on new sites
+	for {
+		fetches := kafkaClient.PollFetches(ctx)
+		if errs := fetches.Errors(); len(errs) > 0 {
+			log.Fatal("fetches had errors: %v\n", errs)
+		}
+		iter := fetches.RecordIter()
+		for !iter.Done() {
+			fmt.Println("received record")
+			record := iter.Next()
+
+			url, err := url.ParseRequestURI(string(record.Value))
+			if err != nil {
+				fmt.Printf("failed to parse URL: %v for record: %v, skipping...\n", err, string(record.Value))
+				continue
+			}
+			handleSiteFromQueue(url.String(), &sitesCollection, kafkaClient)
+		}
+
+	}
 }
